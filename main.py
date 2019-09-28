5c94d76870a8f16b595066a2532a9c1f4a1b18c8
Will Beddow <will@willbeddow.com>
2019-09-28 14:01:01 -0500

    Remove debug print statements from KRLX api

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-# Builtin imports
-
 # Internal/Django imports
 from willbeddow import content_utils
 from django.core.cache import cache
 from django.conf import settings
+from django.http.response import JsonResponse
 
 # External imports
 import twitter
+import spacy
+import requests
+
+# Builtin imports
+import urllib
+
+# Load the spacy model
+nlp = spacy.load('en_core_web_lg')
 
 _CONTENTFUL_FEATURE_ID = "candidate-map-2020"
+_GEOCODING_KEY = settings.CONF_DATA["candidate-map-geocoding-key"]
 # Set up the twitter api
 api = twitter.Api(consumer_key=settings.CONF_DATA["twitter"]["api_key"],
                   consumer_secret=settings.CONF_DATA["twitter"]["secret_key"],
@@ -16,8 +24,17 @@ api = twitter.Api(consumer_key=settings.CONF_DATA["twitter"]["api_key"],
                   access_token_secret=settings.CONF_DATA["twitter"]["access_token_secret"])
 
 
-def get_candidates():
-    pass
+def geocode(location):
+    """
+    Take a location and use the google geocoding API
+
+    :param location: A location string of where we think thge tweet is about
+    :return: (lat, lon) The latitude and longitude of the location, to be displayed with a pin on the map
+    """
+    geocoding_url = f'https://maps.googleapis.com/maps/api/geocode/json?' \
+                    f'address={location}&key={_GEOCODING_KEY}'
+    geocode_data = requests.get(geocoding_url).json()
+    return geocode_data
 
 
 def get_tweets(candidate):
@@ -26,7 +43,29 @@ def get_tweets(candidate):
     :param candidate:
     :return:
     """
-    pass
+    statuses = api.GetUserTimeline(screen_name=candidate["twitter"])
+    # Use NLP to try to figure out where the tweets are
+    for tweet in statuses:
+        if tweet.user.screen_name == candidate["twitter"]:
+            parsed_location = nlp(tweet.text)
+            for ent in parsed_location.ents:
+                if ent.label_ == "GPE":
+                    location = geocode(ent.text)
+                    if "results" in location.keys():
+                        if len(location["results"]) >= 1:
+                            # Look for a country
+                            for address_component in location["results"][0]["address_components"]:
+                                if "country" in address_component["types"] and address_component["short_name"] == "US":
+                                    print(tweet)
+                                    return {
+                                        "tweet": f"https://twitter.com/{candidate['twitter']}/status/{tweet.id}",
+                                        "user_image": tweet.user.profile_image_url_https,
+                                        "lat": location["results"][0]["geometry"]["location"]["lat"],
+                                        "lon": location["results"][0]["geometry"]["location"]["lng"],
+                                        "location": ent.text
+                                    }
+    return None
+
 
 def _get_candidates():
     """
@@ -47,6 +86,11 @@ def candiate_data(request, *args, **kwargs):
     The view endpoint that will return the actual data for the API
     :return:
     """
-    candidates = _get_candidates()
-    for candidate in candidates:
+    candidates_raw = _get_candidates()
+    candidates = []
+    for candidate in candidates_raw:
         candidate_tweets = get_tweets(candidate)
+        if candidate_tweets:
+            candidate.update(candidate_tweets)
+        candidates.append(candidate)
+    return JsonResponse({"candidates": candidates})
